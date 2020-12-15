8d2f6d5d4cd1b81b06a60143358caf203693f5a9
AdedejiArt <71102564+AdedejiArt@users.noreply.github.com>
2020-12-15 05:50:00 -0800

    Create Adio_Sankoh Queries.sql

---
+++ Adio_Sankoh Queries.sql
@@ -0,0 +1,25 @@
+SELECT * FROM `payment` WHERE bill_id=004
+
+SELECT bill_id FROM `payment` WHERE amount_paid='100,000'
+
+SELECT Plant_id FROM plants WHERE Plant_Size < '3,000 Litres'
+
+SELECT Plant_id from route WHERE route_id =6
+
+SELECT NAME FROM `clients` WHERE Zone=001
+
+
+SELECT Name FROM `bills` WHERE bill_id < 4
+
+SELECT Office_physical_address FROM `sector` WHERE Sector_id=5
+
+SELECT Staff_Name FROM `Staff` WHERE Staff_id >4
+
+SELECT Client_id FROM `premises` WHERE Meter_Number=3
+
+SELECT TARRIFF_ID FROM’Charge_Structure’ WHERE Upper_limit=5
+
+SELECT Zone_name FROM ‘Zone WHERE Sector_id=1
+
+
+
